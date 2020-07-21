f2e583698d1be6ff0d0e89cb7b10023b633e0de2
mmnr00 <mmnr00@gmail.com>
2020-07-21 17:31:38 +0800

    tsk

---
+++ app/views/rails_c/newcltid2kc.rb
@@ -0,0 +1,37 @@
+t=Taska.where.not(collection_id2: "x7w_y71n")
+
+arr_main = []
+
+t.each do |tsk|
+arrtmp = [tsk.name,tsk.id,tsk.collection_id2]
+arr_main << arrtmp
+end
+
+[["TADIKA ANNUR SRIKANDI", 277, "pajvgfjy","eiqvnq_x"], 
+["BABY WORLD (TASKA CAHAYA HAZEEQ)", 287, "40bx20ao","xla_rrhg"], 
+["TASKA ANNUR SRIKANDI", 276, "pajvgfjy","eiqvnq_x"], 
+["ANNUR KIDS HOSPITAL BANTING", 286, "pajvgfjy","eiqvnq_x"], 
+["TASKA PERMATA DD", 66, "eaf0w8ws","bviakuts"], 
+["TASKA AMAN DINAMIK ", 275, "clcddhvr","gwl7jfn1"], 
+["TASKA CAHAYA HAZEEQ (GLOMAC)", 55, "40bx20ao","xla_rrhg"], 
+["AS SAKEENAH LEARNING CENTRE", 53, "h3xuyihc","1xrgvccc"], 
+
+["TASKA CAHAYA HAZEEQ (PARKVIEW)", 81, "kq6cxriy"], 
+["TASKA IMPIAN KASIH MURNI", 59, "rmttjlgv"], 
+["TASKA RAEDI", 76, "orjc1zxi"], 
+["TASKA NUR CAHAYA KASIH", 54, "kwkamuzp"], 
+["TASKA CAHAYA HAZEEQ (KASTAM)", 56, "40bx20ao","xla_rrhg"], 
+["KIDCARE DEMO TASKA", 52, "1y3glusm"], 
+["PUSAT JAGAAN CAHAYA HAZEEQ", 80, "kq6cxriy"], 
+["KIDDIE COVE CHILDCARE CENTRE", 574, "ztbusys5"], 
+["TADIKA INTELEK GENIUS SOLEH", 282, "_yoitbi9"], 
+["TASKA NUR ASFA", 72, "zcwcyfc2"], 
+["LITTLE PEOPLE CHILDCARE CENTRE", 592, "tmdot0uh"], 
+["TADIKA ANNAJIYA", 594, "kzexo4nz"], 
+["TASKA KASIH CEMERLANG ", 586, "0ykxvmkg"], 
+["TASKA ANNAJIYA", 593, "sdgppiox"], 
+["TASKA SARJANA PINTAR (SEKSYEN 6)", 46, "nseb_5tl"], 
+["PRECIOUS KIDS MENARA WORLDWIDE", 576, "i0247irc"], 
+["TASKA JULIE", 278, "pq4ou0ad"], 
+["TASKA SARJANA PINTAR (SEKSYEN 7)", 47, "nseb_5tl"], 
+["TASKA MAWADDAH", 292, "nseb_5tl"]]
