bf43c3ff8d7043d30a38b6bdf3553f015f7dadb1
mmnr00 <mmnr00@gmail.com>
2021-01-24 11:27:21 +0800

    tsk

--- app/controllers/vltrs_controller.rb
+++ app/controllers/vltrs_controller.rb
@@ -6,17 +6,36 @@ class VltrsController < ApplicationController
 	end
 
 	def crt_vltr
+		@vltr = Vltr.new(vltr_params)
+		@taska = @vltr.taska
+
+		if Vltr.where(ic: @vltr.ic).present?
+			flash[:danger] = "Data already exist. Please contact admin"
+			redirect_to cakna21_path(volunteer: 1) and return
+		end
+
+		if @vltr.save
+			flash[:success] = "#{@vltr.name} successfully registered to #{@taska.name}"
+		else
+			flash[:danger] = "Volunteer registration failed with error #{@kid.errors.full_messages}"
+		end
+
+		redirect_to cakna21_path(volunteer: 1)
 	end
 
 	def edit_vltr
+		@vltr = Vltr.find(params[:id])
+		@taska = @vltr.taska
 	end
 
 	def upd_vltr
+		@vltr = Vltr.find(params[:vltr][:id])
+		@vltr.update(vltr_params)
 	end
 
 	private
 
-	def kid_params
+	def vltr_params
       params.require(:vltr).permit(:name,
       														 :ic,
       														 :email,
