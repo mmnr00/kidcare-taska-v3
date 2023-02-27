7e94a6dc6fb7eaf3c193566a66d0ea4c15ac524d
mmnr00 <mmnr00@gmail.com>
2023-02-27 20:30:10 +0800

    tsk

--- app/controllers/taskas_controller.rb
+++ app/controllers/taskas_controller.rb
@@ -616,7 +616,8 @@ class TaskasController < ApplicationController
         cdtn_2 = curr_pmt_paid.where('extract(year  from updated_at) = ?', @yr).where('extract(month  from updated_at) = ?', @mth)
         #CDTN_3 = previous period pay this month
         dt_lp = dt
-        stp_lp = Time.find_zone("Singapore").local((Date.today.year -3),1)
+        min_yr = (Date.today - 3.years).year
+        stp_lp = Time.find_zone("Singapore").local(min_yr,1)
         cdtn_3 = nil
         while dt_lp >= stp_lp
           if cdtn_3.blank?    
