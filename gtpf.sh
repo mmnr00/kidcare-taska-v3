d1f1f623515b5356cd554f1f68d5c126b5b64c33
mmnr00 <mmnr00@gmail.com>
2023-10-10 08:15:49 +0800

    tsk

--- gtpf.sh
+++ gtpf.sh
@@ -4,14 +4,14 @@ git add -A
 git commit -m "tsk"
 git push
 
-git checkout stg_n
+git checkout 10stg
 git merge 10dev
 git push
 
 git checkout production_n
-git merge stg_n
+git merge 10stg
 git push
 
-git diff HEAD stg_n
+git diff HEAD 10stg
 git diff HEAD 10dev
 git checkout 10dev
