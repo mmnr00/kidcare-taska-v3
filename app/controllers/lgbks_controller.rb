6aeafed8462dab603e76cd8bf53e897b2135d0ff
mmnr00 <mmnr00@gmail.com>
2024-01-10 08:15:26 +0800

    tsk

--- app/controllers/lgbks_controller.rb
+++ app/controllers/lgbks_controller.rb
@@ -157,7 +157,7 @@ class LgbksController < ApplicationController
 			lg_curr = lgbk.last
 		end
 		lg_curr.cin << Time.now
-		lg_curr.cin << tch.id
+		lg_curr.cin << "tch.id"
 		lg_curr.cin << params[:tmpi]
 		lg_curr.save
 		#flash[:notice] = "Check-in successful for #{kid.name} by #{tch.username}"
@@ -169,7 +169,7 @@ class LgbksController < ApplicationController
 		kid = lg_curr.kid
 		tch = Teacher.find(params[:tch])
 		lg_curr.cout << Time.now
-		lg_curr.cout << tch.id
+		lg_curr.cout << "tch.id"
 		lg_curr.cout << params[:tmpo]
 		lg_curr.save
 		#flash[:notice] = "Check-Out successful for #{kid.name} by #{tch.username}"
