40978d0d35b2f00ff0a9461f134bac0bab95e2ff
mmnr00 <mmnr00@gmail.com>
2025-07-09 07:46:32 +0800

    tsk

--- app/controllers/lgbks_controller.rb
+++ app/controllers/lgbks_controller.rb
@@ -194,13 +194,13 @@ class LgbksController < ApplicationController
 	def std_checkin
 		kid = Kid.find(params[:kid])
 		#tch = Teacher.find(params[:tch])
-		lgbk = kid.lgbks.where(created_at: Time.now.beginning_of_day..Time.now.end_of_day)
+		lgbk = kid.lgbks.where(created_at: $my_time.beginning_of_day..$my_time.end_of_day)
 		if lgbk.blank?
 			lg_curr = Lgbk.new(kid_id: kid.id, taska_id: kid.taska.id)
 		else
 			lg_curr = lgbk.last
 		end
-		lg_curr.cin << Time.now
+		lg_curr.cin << $my_time
 		lg_curr.cin << "tch.id"
 		lg_curr.cin << params[:tmpi]
 		lg_curr.save
@@ -212,7 +212,7 @@ class LgbksController < ApplicationController
 		lg_curr = Lgbk.find(params[:lgbk])
 		kid = lg_curr.kid
 		#tch = Teacher.find(params[:tch])
-		lg_curr.cout << Time.now
+		lg_curr.cout << $my_time
 		lg_curr.cout << "tch.id"
 		lg_curr.cout << params[:tmpo]
 		lg_curr.save
